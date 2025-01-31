34cba30b6474966a4fff2f75ad9c3e1ffa430696
Anthony Li <me@anli.dev>
2025-01-31 11:48:44 -0500

    Change blog title styling

--- src/routes/blog/[slug]/+page.svelte
+++ src/routes/blog/[slug]/+page.svelte
@@ -1,6 +1,9 @@
 <script lang="ts">
 	import type { Post } from '$lib';
 
+	// @ts-ignore
+	import IconBack from '~icons/majesticons/arrow-circle-left';
+
 	export interface BlogPostData {
 		post: Post;
 	}
@@ -12,13 +15,28 @@
 	<div
 		class="container mx-auto flex flex-col md:flex-row items-center md:items-start mt-8 md:mt-12 mb-12"
 	>
-		<div class="flex-shrink-0 md:w-52 md:mx-12">
-			<h1 class="mb-8 font-sans text-6xl font-bold text-white text-center">Blog</h1>
+		<div class="flex-shrink-0 md:w-52 md:mx-12 pb-12 text-center text-white">
+			<div class="md:h-12 mb-6 md:mb-0 flex items-start justify-center">
+				<a
+					href="/blog"
+					class="flex items-center justify-center gap-2 w-fit px-4 py-2 md:-mt-2 opacity-70 bg-white/0 hover:bg-white/10 active:bg-white/20 focus:bg-white/10 rounded-xl transition-colors"
+					><IconBack class="mt-0.5" /> <span class="underline">All Posts</span></a
+				>
+			</div>
+			<h1 class="font-sans text-4xl font-bold mb-2 md:mb-6 break-words">
+				{data.post.title}
+			</h1>
+			<div class="opacity-80">
+				{data.post.date.toLocaleDateString('en-US', {
+					year: 'numeric',
+					month: 'long',
+					day: 'numeric'
+				})}
+			</div>
 		</div>
 		<div
-			class="w-full flex-grow rounded-3xl bg-white dark:bg-ocean-950 px-8 py-12 md:px-12 dark:contrast-more:bg-black"
+			class="w-full flex-grow rounded-3xl bg-white dark:bg-ocean-950 px-8 py-8 md:px-12 dark:contrast-more:bg-black"
 		>
-			<h1 class="font-sans text-5xl font-bold mb-8">{data.post.title}</h1>
 			<div class="prose-anli">
 				<data.post.content />
 			</div>
