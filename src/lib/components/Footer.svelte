5c263cd2d9e7d9a181a495d70f0fff4964afbb95
Anthony Li <me@anli.dev>
2025-02-02 22:03:10 -0500

    Update footer

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -1,4 +1,7 @@
 <script lang="ts">
+    // @ts-ignore
+    import IconHeart from '~icons/majesticons/heart';
+
 	interface Props {
 		becomesTransparent?: boolean;
 	}
@@ -7,15 +10,16 @@
 
 	const transparentizer = $derived(
 		becomesTransparent
-			? 'sm:dark:bg-transparent sm:dark:contrast-more:bg-transparent sm:text-white/70'
+			? 'sm:dark:bg-transparent sm:dark:contrast-more:bg-transparent sm:text-white'
 			: ''
 	);
 </script>
 
 <div class="dark:bg-ocean-950 dark:contrast-more:bg-black {transparentizer}">
 	<footer
-		class="rounded-t-2xl py-12 text-center text-ocean-950/60 dark:text-white/70 w-full dark:bg-ocean-1000 dark:contrast-more:bg-black {transparentizer}"
+		class="rounded-t-2xl px-4 py-12 text-center text-ocean-950/60 dark:text-white w-full dark:bg-ocean-1000 dark:contrast-more:bg-black {transparentizer}"
 	>
-		&copy; 2025 Anthony Li
+		<p class="mb-2">&copy; 2025 Anthony Li • <a class="opacity-70 hover:underline hover:opacity-60" href="https://github.com/anli5005/site-svelte">Made with ❤️</p>
+        <p><a class="underline opacity-70 hover:opacity-60" href="https://github.com/anli5005/site-svelte/blob/main/package.json">Powered by SvelteKit and many other open-source projects</a></p>
 	</footer>
 </div>
