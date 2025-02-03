f1c665ee43d0d757f6c2e767ce0ad5fd70410f9e
Anthony Li <me@anli.dev>
2025-02-02 23:50:53 -0500

    Fix prerendering error 3

--- src/routes/classnotes/[...path]/+server.ts
+++ src/routes/classnotes/[...path]/+server.ts
@@ -1,7 +1,5 @@
 import { redirect, type RequestHandler } from "@sveltejs/kit";
 
-export const prerender = true;
-
 export const GET: RequestHandler<{ path: string }> = async ({ params }) => {
     redirect(303, `https://bcanotes.anli.dev/${params.path}`);
 };
