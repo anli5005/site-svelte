8c27e296f72bf5858eb2610da313eb4c25b13d8f
Anthony Li <me@anli.dev>
2025-02-02 23:50:22 -0500

    Fix prerendering error 2

--- src/routes/Sorts/+server.ts
+++ src/routes/Sorts/+server.ts
@@ -3,5 +3,5 @@ import { redirect, type RequestHandler } from "@sveltejs/kit";
 export const prerender = true;
 
 export const GET: RequestHandler = async ({ url }) => {
-    redirect(303, `/Sorts/index.html?${url.searchParams}`);
+    redirect(303, `/Sorts/index.html`);
 };
